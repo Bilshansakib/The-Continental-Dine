d21091da8f880a0ecf92a4c4403eeb6e0b711991
Bilshansakib <burakaghani@gmail.com>
2024-05-10 03:16:54 +0600

    Top foods section added

---
+++ src/components/TopFoods.jsx
@@ -0,0 +1,18 @@
+const TopFoods = () => {
+  return (
+    <div className="lg:flex  w-full">
+      <div className="w-full p-8 grid-flow-row grid border-2 bg-slate-100 h-96">
+        <div className="">card</div>
+        <div className="">a</div>
+        <div className="">a</div>
+      </div>
+      <div className="w-full p-8 grid-flow-row grid border-2 bg-slate-100 h-96">
+        <div className="">card</div>
+        <div className="">b</div>
+        <div className="">b</div>
+      </div>
+    </div>
+  );
+};
+
+export default TopFoods;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Carousal from "../components/Carousal";
+import TopFoods from "../components/TopFoods";
 
 const Home = () => {
   return (
     <div>
       <Carousal></Carousal>
+      <TopFoods></TopFoods>
     </div>
   );
 };
